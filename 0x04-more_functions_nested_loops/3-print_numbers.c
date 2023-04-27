b96d26abff1dfd0a1250bbd0c25b54129f9eb639
myname <snwoleymorgan95@gmail.com>
2023-04-27 09:16:51 -0700

    a function that prints the numbers, from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_numbers - print all natural numbers between 0 and 9
+ * followed by new line
+ * return: void
+*/
+
+
+void print_numbers(void)
+
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+}
