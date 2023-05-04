82a7cff751332a799c753681c16071523dc8fa99
myname <snwoleymorgan95@gmail.com>
2023-05-04 11:05:52 -0700

    add one line of code

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-  * main -Entry point
+  * main - Entry point
   *
-  * Return: 0
+  * Return:0
 */
 
 int main(void)
@@ -13,8 +13,17 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-	*(p + 4) = 98; /* add the value 98 to the memory allocation pointed by p+4 */
-	printf("a[2] = %d\n", a[2]); /* print the value of a[2] */
-
+	/*
+	 * write your line of code here...
+	 * Rememner:
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
+	 */
+	p[5] = 98;
+	/* ...so that prints 98\n */
+	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
+
