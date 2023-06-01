aaab4efc33b2a838071ec536d51cfec4835d3a51
Sanveille95 <snwoleymorgan95@gmail.com>
2023-06-01 10:37:08 -0700

    Updated

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,19 @@
-	global main
-	extern printf
+section .data
+format db "%s", 10, 0
+message db "Hello, Holberton", 0
+
+section .text
+extern printf ;C function to be called
+global main ;main function
+
 main:
-	mov edi, format
-	xor eax, eax
-	call printf
-	mov eax, 0
-	ret
-format: db'Hello, Holberton\n',0
+	push rbp
+	mov rdi, format
+	mov rsi, message
+	mov rax, 0 ;can be xor rax, rax
+	call printf ;call C function.
+
+	pop rbp ;restor stack
+
+	mov rax, 0 ;normal, no error, return value
+	ret ;return
