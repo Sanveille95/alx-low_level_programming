29dc4dd36f00eec68ba2ceb5fe8de43a2759947c
Sanveille95 <snwoleymorgan95@gmail.com>
2023-06-06 04:52:52 -0700

    update to free memory allocated in the list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,17 +8,12 @@
 
 void free_listint(listint_t *head)
 {
-	if (head == NULL)
-	{
-		return;
-	}
+	listint_t *tmp;
 
-	while (head != NULL)
+	while ((tmp = head) != NULL)
 	{
-		listint_t *tmp;
-
-		tmp = head;
-		free(tmp);
 		head = head->next;
+		free(tmp);
+
 	}
 }
