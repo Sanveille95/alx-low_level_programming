7ff5ed21575d595a07b5bd5b41a5542d938aab28
sanveille95 <snwoleymorgan95@gmail.com>
2023-07-10 13:23:17 -0700

    copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,88 +1,46 @@
 #include "main.h"
 
 /**
- * create_buffer - Allocates 1024 bytes for a buffer
- * @file: file name
- * Return: A pointer to the buffer
- */
-char *create_buffer(char *file)
+ * main - cp
+ * @argc: must be 3 always
+ * @argv: file_from file_to
+ * Return: 0 ok, error is something go wrong
+ **/
+int main(int argc, char **argv)
 {
-	char *buffer;
-
-	buffer = malloc(sizeof(char) * 1024);
-
-	if (buffer == NULL)
-	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't write to %s\n", file);
-		exit(99);
-	}
-
-	return (buffer);
-}
-
-/**
- * close_file - Closes file descriptors.
- * @fd: The file descriptor to be closed.
- */
-void close_file(int fd)
-{
-	int count;
-
-	count = close(fd);
-
-	if (count == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
-}
-
-/**
- * main - copies the contents from file to another
- * @argc: number of arguments
- * @argv: array of pointers
- * Return: 0 on success
- */
-int main(int argc, char *argv[])
-{
-	int file_from, file_to, toread, towrite;
-	char *buffer;
+	int fd_from, fd_to, to_close, from_close, from_read, to_write;
+	char buff[BUF1024];
 
 	if (argc != 3)
+		dprintf(STDERR_FILENO, FILE_USAGE), exit(97);
+
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+		dprintf(STDERR_FILENO, READ_ERR, argv[1]), exit(98);
+
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		from_close = close(fd_from);
+		if (from_close)
+			dprintf(STDERR_FILENO, ERR_100, fd_from), exit(100);
+		dprintf(STDERR_FILENO, WRITE_ERR, argv[2]), exit(99);
 	}
 
-	buffer = create_buffer(argv[2]);
-	file_from = open(argv[1], O_RDONLY);
-	toread = read(file_from, buffer, 1024);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((from_read = read(fd_from, buff, BUF1024)) != 0)
+	{
+		if (from_read == -1)
+			dprintf(STDERR_FILENO, READ_ERR, argv[1]), exit(98);
+		to_write = write(fd_to, buff, from_read);
+		if (to_write == -1)
+			dprintf(STDERR_FILENO, WRITE_ERR, argv[2]), exit(99);
+	}
+	from_close = close(fd_from);
+	if (from_close)
+		dprintf(STDERR_FILENO, ERR_100, fd_from), exit(100);
 
-	do {
-		if (file_from == -1 || toread == -1)
-		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't read from file %s\n", argv[1]);
-			free(buffer);
-			exit(98);
-		}
-
-		towrite = write(file_to, buffer, toread);
-		if (file_to == -1 || towrite == -1)
-		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't write to %s\n", argv[2]);
-			free(buffer);
-			exit(99);
-		}
-
-		toread = read(file_from, buffer, 1024);
-
-	} while (toread > 0);
-	free(buffer);
-	close(file_from);
-	close(file_to);
+	to_close = close(fd_to);
+	if (to_close == -1)
+		dprintf(STDERR_FILENO, ERR_100, fd_to), exit(100);
 	return (0);
 }
